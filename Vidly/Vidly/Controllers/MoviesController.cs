9a4e53d7c91b7eaeb05399cd1bcaaf13fd8ec1d2
Niall <niall.mccafferty@connollys.ie>
2020-04-18 00:22:14 +0100

    Added routing Added Customer class Added ViewModels folder and class Extracted NavBar into a Partial View Returned viewModel from Movies() in Movies controller

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,9 +13,30 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Movies()
         {
-            var movie = new Movie() { Name = "Shrek", Genre = "Comedy" };
+            var movie = new Movie() { Name = "Matrix", Genre = "Comedy" };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 2"}
+            };
+
+            var viewModel = new MovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+  
+            return View(viewModel);
         }
+ 
+
+        [Route("movies/released/{year:regex(\\d{4}):range(1900, 2050)}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
     }
 }
